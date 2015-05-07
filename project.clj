6350052aa4424aa08fad979f9a61401022851398
Pedro Santos <donbonifacio@gmail.com>
2015-05-07 11:55:45 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/validators "0.1.0-SNAPSHOT"
+(defproject clanhr/validators "0.1.0"
   :description "Utility model validators that integrate with validateur"
   :url "https://github.com/clanhr/validators"
   :license {:name "Eclipse Public License"
